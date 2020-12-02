edeb55a18968585e1124f299e3b56f2c8f243111
May Jhao <fleetinghappines@gmail.com>
2020-12-02 16:46:49 +0800

    Change validate rule in ProfileController

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Profile;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rule;
 
 class ProfileController extends Controller
 {
@@ -34,7 +35,9 @@ class ProfileController extends Controller
     {
         return $request->validate([
             'name' => ['required', 'string'],
-            'email' => ['required', 'email', 'max:255', 'exists:users,email'],
+            'email' => ['required', 'email', 'max:255',
+            Rule::unique('users','email')->ignore(Auth::id()) 
+        ],
             'password' => ['required','min:8', 'confirmed']
         ]);
     }
